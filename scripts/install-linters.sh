2dd0ba7271e2950a2d45f7b9faa2bebd7a67902e
Sebastian Kreft <skreft@gmail.com>
2014-09-08 19:36:59 +0200

    Install ruby with rvm and node from nodesource

--- scripts/install-linters.sh
+++ scripts/install-linters.sh
@@ -1,13 +1,19 @@
 sudo apt-get remove rubygems ruby
-sudo apt-get install curl build-essential php-pear optipng pngcrush php5 ruby1.9.3 checkstyle libjpeg-turbo-progs xsltproc
+sudo apt-get install curl build-essential php-pear optipng pngcrush php5 checkstyle libjpeg-turbo-progs xsltproc
 #sudo apt-get install nodejs-legacy in ubuntu 14
 
+# Install ruby
+curl -sSL https://get.rvm.io | bash -s stable --ruby=1.9.3
+source $HOME/.rvm/scripts/rvm
+
 # Install latest node
-curl http://nodejs.org/dist/node-latest.tar.gz | tar xz
-cd node-v*
-./configure
-sudo make install
-cd -
+#curl http://nodejs.org/dist/node-latest.tar.gz | tar xz
+#cd node-v*
+#./configure
+#sudo make install
+#cd -
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install nodejs
 
 # Install npm
 curl -L https://npmjs.org/install.sh | sh
